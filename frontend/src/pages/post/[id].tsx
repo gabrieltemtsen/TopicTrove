df42750d2597f3407c6b67563df5b86aeb8ea61d
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-01-18 12:53:26 +0100

    UI: fix

--- frontend/src/pages/post/[id].tsx
+++ frontend/src/pages/post/[id].tsx
@@ -1,5 +1,5 @@
 import HeaderNav from '@/components/HeaderNav'
-import { Avatar, Box, Center, Container, Flex, Heading, Icon, PinInput, PinInputField, Square, Text, Wrap, WrapItem } from '@chakra-ui/react'
+import { Avatar, Box, Center, Container, Divider,Image, Flex, Heading, Icon, PinInput, PinInputField, Square, Text, Wrap, WrapItem } from '@chakra-ui/react'
 
 import React from 'react'
 
@@ -16,36 +16,89 @@ const Post = () => {
   return (
 
     <>
-      <HeaderNav/>
-
     
+    <HeaderNav bg='transparent'/>
+
+    <section id='Title' className='post-title-section'>
+
+    <Box  display={'flex'} flexDirection={'column'} gap={16} mb={2} pt={8} pb={40} maxW={'1050px'} h={345} alignItems={'flex-start'} mt={'45px'} ml={'170px'}>
+
+<Heading fontSize={'40px'} fontWeight={700} lineHeight={'normal'} >The Astonishing Origins of 6 Common Compound Words</Heading>
+
+<Box mt={-5} mb={-9}  h={82}  fontWeight={400} fontSize={18} lineHeight={'140%'} maxW={'900px'} fontStyle={'normal'} color='#636566'>
+The 1989 Mill Valley Film Festival, in association with the Hanson Gallery in 
+neighboring Sausalito, installed an exhibition of my portraits, including movie directors, 
+actors, and writers. I was billed as a “Featured Artist”
+
+<Divider mt={5} mb={2} orientation='horizontal' />
+</Box>
+
+
+<Box display={'flex'}  mt={2} mb={-5} alignItems={'flex-start'} gap={'20px'} alignSelf={'stretch'}>
+<Avatar size='md' name='Christian Nwamba' src='https://bit.ly/code-beast' />
+<Box display={'flex'} flexDirection={'column'} alignItems={'flex-start'} gap={'7px'}>
+<Text fontWeight={500} lineHeight={'normal'} fontSize={'20px'}>Mark Andre</Text>
+
+<Text color='#636566' fontWeight={400} lineHeight={'normal'} fontSize={'16px'}>8 min read <CircleIcon ml={4} mr={2} color={'#636566'} boxSize={1.5} /> Jan 18 2024</Text>
+
+</Box>
+</Box>
+<Divider orientation='horizontal' />
+
+</Box>
+    </section>
+
+
+
+<section className='post-image'>
+<Box >
+<Image src='https://bit.ly/48UyfzN' alt='Dan Abramov' h={'640px'}  className='w-full' objectFit={'cover'}/>
+</Box>
+
+</section>
+
+
+<section className='post-content'>
+<Text color={'black'} h={'176px'} fontSize={18} flexShrink={0} fontWeight={400} alignSelf={'stretch'} >
+A week or so earlier, I was in the gallery with the staff,
+putting our heads together about how to hang the
+installation, when it dawned on us that I’d never
+photographed either Woods or Richardson. Unfortunately, 
+they weren’t available to sit for portraits before the festival 
+kicked off. But it might seem downright negligent to gallery visitors, 
+we thought, that I’d never photographed one of the most illustrious 
+cinemagicians of them all, a practically home-grown hero who lived and 
+worked not so far, far away: George Lucas. Skywalker Ranch, the Lucasfilm
+production facility, was a short drive from the gallery.
+</Text>
+
+</section>
+
+
+
+<Box  ml={'170px'}  h={'202px'} mb={9}>
+
+<Box display={'inline-flex'}  flexDirection={'column'} alignItems={'flex-start'} gap={'10px'} padding={'2px 79px 35px 0px'}>
+<Avatar flexShrink={0} size='md' name='Christian Nwamba' src='https://bit.ly/code-beast' />{' '}
+
+<Text color={'black'} fontSize={24} fontWeight={600}>Written By Mark Andre</Text>
+    <Text color={'636566'} fontWeight={400} fontSize={16}> This is my Bio</Text>
+</Box>
+
+</Box>
+
+
+     
 
       
 
-        <Box display={'flex'} flexDirection={'column'} gap={16} flexShrink={0} w={'1050px'} pt={8} pb={40} maxW={'1050px'} h={345} alignItems={'flex-start'} mt={'45px'} ml={'170px'}>
 
-        <Heading fontSize={'40px'} fontWeight={700} lineHeight={'normal'} size={'lg'}>The Astonishing Origins of 6 Common Compound Words</Heading>
-  
-        <Box mt={-5} mb={-6} flexShrink={0} h={82} w={738} fontWeight={400} fontSize={18} lineHeight={'140%'} fontStyle={'normal'} color='#636566'>
-        The 1989 Mill Valley Film Festival, in association with the Hanson Gallery in 
-        neighboring Sausalito, installed an exhibition of my portraits, including movie directors, 
-        actors, and writers. I was billed as a “Featured Artist”
-        </Box>
 
-        <Box display={'flex'} alignItems={'center'} gap={'24px'} alignSelf={'stretch'}>
-        <Avatar size='md' name='Christian Nwamba' src='https://bit.ly/code-beast' />{' '}
-        <Box display={'flex'} flexDirection={'column'} alignItems={'flex-start'} gap={'7px'}>
-            <Text fontWeight={500} lineHeight={'normal'} fontSize={'20px'}>Mark Andre</Text>
+     
 
-            <Text color='#636566' fontWeight={400} lineHeight={'normal'} fontSize={'16px'}>8 min read <CircleIcon ml={4} mr={2} color={'#636566'} boxSize={1.5} /> Jan 18 2024</Text>
+      
 
-        </Box>
-           
-            
-        </Box>
-
-        </Box>
-  
+        
 
 
 
