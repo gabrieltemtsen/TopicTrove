6a584bbff48e90d6968fe64cb2c3571d097414a2
degencodebeast <degencodebeast@protonmail.com>
2024-01-19 23:27:22 +0100

    feat: added connectkit btn to nav

--- frontend/src/components/HeaderNav.tsx
+++ frontend/src/components/HeaderNav.tsx
@@ -1,17 +1,29 @@
-import { Button, Flex } from "@chakra-ui/react"
-import Logo from "./Logo"
-import Nav from "./Nav"
+import { Button, Flex } from "@chakra-ui/react";
+import Logo from "./Logo";
+import Nav from "./Nav";
+import { ConnectKitButton } from "connectkit";
 
-interface HeaderProps{
-    bg?:string
+interface HeaderProps {
+    bg?: string;
 }
-export default function HeaderNav<HeaderProps>({bg='appGreen'}){
-
-    return <Flex borderBottom={'1px'} borderBottomColor={'appBlack.900'} bg={bg} className="" px={8} py={4} align={'center'} justify={'space-between'}>
-<Logo/>
-<Nav/>
-<Flex>
-    <Button rounded={'full'} size={'lg'}>Connect Wallet</Button>
-</Flex>
-    </Flex>
+export default function HeaderNav<HeaderProps>({ bg = "appGreen" }) {
+    return (
+        <Flex
+            borderBottom={"1px"}
+            borderBottomColor={"appBlack.900"}
+            bg={bg}
+            className=""
+            px={8}
+            py={4}
+            align={"center"}
+            justify={"space-between"}
+        >
+            <Logo />
+            <Nav />
+            <Flex>
+                {/* <Button rounded={'full'} size={'lg'}>Connect Wallet</Button> */}
+                <ConnectKitButton />
+            </Flex>
+        </Flex>
+    );
 }
